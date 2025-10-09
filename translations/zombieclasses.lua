bdcc79fca8f5f1eb9d16645658d6f543a32724e9
crack <63127055+Crack031@users.noreply.github.com>
2025-10-09 04:12:14 -0400

    add zombieclasses localization

---
+++ translations/zombieclasses.lua
@@ -0,0 +1,740 @@
+-- Zombie Classes
+
+translate.AddLocalization("class_zombie", {
+    ["en"] = "Zombie"
+})
+translate.AddLocalization("class_zombine", {
+    ["en"] = "Zombine"
+})
+translate.AddLocalization("class_zombie_ichor", {
+    ["en"] = "Ichor Zombie"
+})
+translate.AddLocalization("class_poison_zombie", {
+    ["en"] = "Poison Zombie"
+})
+translate.AddLocalization("class_wild_poison_zombie", {
+    ["en"] = "Wild Poison Zombie"
+})
+translate.AddLocalization("class_alpha_poison_zombie", {
+    ["en"] = "Alpha Poison Zombie"
+})
+translate.AddLocalization("class_fast_zombie", {
+    ["en"] = "Fast Zombie"
+})
+translate.AddLocalization("class_violator", {
+    ["en"] = "Violator"
+})
+translate.AddLocalization("class_crimson_render", {
+    ["en"] = "Crimson Render"
+})
+translate.AddLocalization("class_fast_zombie_slingshot", {
+    ["en"] = "Slingshot Zombie"
+})
+translate.AddLocalization("class_bloated_zombie", {
+    ["en"] = "Bloated Zombie"
+})
+translate.AddLocalization("class_vile_monster", {
+    ["en"] = "Vile Monster"
+})
+translate.AddLocalization("class_turgid_wretch", {
+    ["en"] = "Turgid Wretch"
+})
+translate.AddLocalization("class_classic_zombie", {
+    ["en"] = "Classic Zombie"
+})
+translate.AddLocalization("class_super_zombie", {
+    ["en"] = "Super Zombie"
+})
+translate.AddLocalization("class_fresh_dead", {
+    ["en"] = "Fresh Dead"
+})
+translate.AddLocalization("class_recent_dead", {
+    ["en"] = "Recent Dead"
+})
+translate.AddLocalization("class_agile_dead", {
+    ["en"] = "Agile Dead"
+})
+translate.AddLocalization("class_initial_dead", {
+    ["en"] = "Initial Dead"
+})
+translate.AddLocalization("class_ghoul", {
+    ["en"] = "Ghoul"
+})
+translate.AddLocalization("class_chilled_ghoul", {
+    ["en"] = "Frigid Ghoul"
+})
+translate.AddLocalization("class_elderghoul", {
+    ["en"] = "Elder Ghoul"
+})
+translate.AddLocalization("class_venerableghoul", {
+    ["en"] = "Venerable Ghoul"
+})
+translate.AddLocalization("class_frostbittenghoul", {
+    ["en"] = "Frostbitten Ghoul"
+})
+translate.AddLocalization("class_noxiousghoul", {
+    ["en"] = "Noxious Ghoul"
+})
+translate.AddLocalization("class_primeghoul", {
+    ["en"] = "Prime Ghoul"
+})
+translate.AddLocalization("class_primalghoul", {
+    ["en"] = "Primal Ghoul"
+})
+translate.AddLocalization("class_bigghoul", {
+    ["en"] = "Big Ghoul"
+})
+translate.AddLocalization("class_cinder_ghoul", {
+    ["en"] = "Cinder Ghoul"
+})
+translate.AddLocalization("class_thunder_ghoul", {
+    ["en"] = "Thunder Ghoul"
+})
+translate.AddLocalization("class_war_ghoul", {
+    ["en"] = "War Ghoul"
+})
+translate.AddLocalization("class_witheredghoul", {
+    ["en"] = "Withered Ghoul"
+})
+translate.AddLocalization("class_cosmic_ghoul", {
+    ["en"] = "Cosmic Ghoul"
+})
+translate.AddLocalization("class_cool_ghoul", {
+    ["en"] = "Chill Ghoul"
+})
+translate.AddLocalization("class_headcrab", {
+    ["en"] = "Headcrab"
+})
+translate.AddLocalization("class_broodcrab", {
+    ["en"] = "Brood Crab"
+})
+translate.AddLocalization("class_eliminatorcrab", {
+    ["en"] = "Eliminator Crab"
+})
+translate.AddLocalization("class_fast_headcrab", {
+    ["en"] = "Fast Headcrab"
+})
+translate.AddLocalization("class_bloodsucker_headcrab", {
+    ["en"] = "Bloodsucker Crab"
+})
+translate.AddLocalization("class_crimson_headcrab", {
+    ["en"] = "Crimson Headcrab"
+})
+translate.AddLocalization("class_poison_headcrab", {
+    ["en"] = "Poison Headcrab"
+})
+translate.AddLocalization("class_barbed_headcrab", {
+    ["en"] = "Barbed Headcrab"
+})
+translate.AddLocalization("class_carrion_headcrab", {
+    ["en"] = "Carrion Headcrab"
+})
+translate.AddLocalization("class_doomcrab", {
+    ["en"] = "Doom Crab"
+})
+translate.AddLocalization("class_extinctioncrab", {
+    ["en"] = "Extinction Crab"
+})
+translate.AddLocalization("class_alphacrab", {
+    ["en"] = "Alpha Crab"
+})
+translate.AddLocalization("class_headcrab_hatchling", {
+    ["en"] = "Headcrab Hatchling"
+})
+translate.AddLocalization("class_cinder_crab", {
+    ["en"] = "Cinder Crab"
+})
+translate.AddLocalization("class_friendly_headcrab", {
+    ["en"] = "Friendly Headcrab"
+})
+translate.AddLocalization("class_the_tickle_monster", {
+    ["en"] = "The Tickle Monster"
+})
+translate.AddLocalization("class_nightmare", {
+    ["en"] = "Nightmare"
+})
+translate.AddLocalization("class_fleshfire", {
+    ["en"] = "Flesh Fire"
+})
+translate.AddLocalization("class_darkmare", {
+    ["en"] = "Darkmare"
+})
+translate.AddLocalization("class_shadowmare", {
+    ["en"] = "Shadowmare"
+})
+translate.AddLocalization("class_ancient_nightmare", {
+    ["en"] = "Ancient Nightmare"
+})
+translate.AddLocalization("class_tickle_nightmare", {
+    ["en"] = "Tickle Nightmare"
+})
+translate.AddLocalization("class_tickle_torment", {
+    ["en"] = "Tickle Torment"
+})
+translate.AddLocalization("class_tickle_minion", {
+    ["en"] = "Tickle Minion"
+})
+translate.AddLocalization("class_tickledecimator", {
+    ["en"] = "Tickle Decimator"
+})
+translate.AddLocalization("class_shade", {
+    ["en"] = "Shade"
+})
+translate.AddLocalization("class_frostshade", {
+    ["en"] = "Frost Shade"
+})
+translate.AddLocalization("class_gangmare", {
+    ["en"] = "Gangmare"
+})
+translate.AddLocalization("class_devourer", {
+    ["en"] = "Devourer"
+})
+translate.AddLocalization("class_abductor", {
+    ["en"] = "Abductor"
+})
+translate.AddLocalization("class_pukepus", {
+    ["en"] = "Pukepus"
+})
+translate.AddLocalization("class_gunkpus", {
+    ["en"] = "Gunk Pus"
+})
+translate.AddLocalization("class_bilemire", {
+    ["en"] = "Bilemire"
+})
+translate.AddLocalization("class_pyropus", {
+    ["en"] = "Pyro Pus"
+})
+
+translate.AddLocalization("class_bonemesh", {
+    ["en"] = "Bonemesh"
+})
+translate.AddLocalization("class_tarmesh", {
+    ["en"] = "Tarmesh"
+})
+translate.AddLocalization("class_bones", {
+    ["en"] = "Bones"
+})
+translate.AddLocalization("class_crow", {
+    ["en"] = "Crow"
+})
+translate.AddLocalization("class_wilowisp", {
+    ["en"] = "Wil O' Wisp"
+})
+translate.AddLocalization("class_coolwisp", {
+    ["en"] = "Cool Wisp"
+})
+translate.AddLocalization("class_wraith", {
+    ["en"] = "Wraith"
+})
+translate.AddLocalization("class_tormented_wraith", {
+    ["en"] = "Tormented Wraith"
+})
+translate.AddLocalization("class_spectre", {
+    ["en"] = "Spectre"
+})
+translate.AddLocalization("class_banshee", {
+    ["en"] = "Banshee"
+})
+translate.AddLocalization("class_wintergeist", {
+    ["en"] = "Wintergeist"
+})
+translate.AddLocalization("class_spirit_crawler", {
+    ["en"] = "Spirit Crawler"
+})
+translate.AddLocalization("class_spirit_stalker", {
+    ["en"] = "Spirit Stalker"
+})
+translate.AddLocalization("class_stalker", {
+    ["en"] = "Stalker"
+})
+
+translate.AddLocalization("class_zombie_torso", {
+    ["en"] = "Zombie Torso"
+})
+translate.AddLocalization("class_brood_zombie_torso", {
+    ["en"] = "Brood Zombie Torso"
+})
+translate.AddLocalization("class_zombine_torso", {
+    ["en"] = "Zombine Torso"
+})
+translate.AddLocalization("class_zombie_legs", {
+    ["en"] = "Zombie Legs"
+})
+translate.AddLocalization("class_brood_zombie_legs", {
+    ["en"] = "Brood Zombie Legs"
+})
+translate.AddLocalization("class_fast_zombie_legs", {
+    ["en"] = "Fast Zombie Legs"
+})
+translate.AddLocalization("class_fast_zombie_torso", {
+    ["en"] = "Fast Zombie Torso"
+})
+translate.AddLocalization("class_fast_zombie_torso_slingshot", {
+    ["en"] = "Slingshot Zombie Torso"
+})
+translate.AddLocalization("class_gunk_zombie_torso", {
+    ["en"] = "Gunk Zombie Torso"
+})
+translate.AddLocalization("class_gunk_fiend_torso", {
+    ["en"] = "Gunk Fiend Torso"
+})
+translate.AddLocalization("class_gunk_zombie_legs", {
+    ["en"] = "Gunk Zombie Legs"
+})
+translate.AddLocalization("class_gunk_fiend_legs", {
+    ["en"] = "Gunk Fiend Legs"
+})
+translate.AddLocalization("class_restless_zombie_torso", {
+    ["en"] = "Restless Zombie Torso"
+})
+
+translate.AddLocalization("class_chem_fiend", {
+    ["en"] = "Chem Fiend"
+})
+translate.AddLocalization("class_chem_burster", {
+    ["en"] = "Chem Breacher"
+})
+translate.AddLocalization("class_chem_jaeger", {
+    ["en"] = "Chem Jaeger"
+})
+translate.AddLocalization("class_chem_charger", {
+    ["en"] = "Chem Charger"
+})
+translate.AddLocalization("class_chem_zombie", {
+    ["en"] = "Chem Zombie"
+})
+translate.AddLocalization("class_chemjuggernaut", {
+    ["en"] = "Chem Juggernaut"
+})
+translate.AddLocalization("class_butcher", {
+    ["en"] = "The Butcher"
+})
+translate.AddLocalization("class_gravedigger", {
+    ["en"] = "The Grave Digger"
+})
+translate.AddLocalization("class_flesh_creeper", {
+    ["en"] = "Flesh Creeper"
+})
+translate.AddLocalization("class_flesh_weaver", {
+    ["en"] = "Flesh Weaver"
+})
+translate.AddLocalization("class_gore_child", {
+    ["en"] = "Gore Child"
+})
+translate.AddLocalization("class_giga_gore_child", {
+    ["en"] = "Giga Gore Child"
+})
+translate.AddLocalization("class_shadow_gore_child", {
+    ["en"] = "Shadow Child"
+})
+translate.AddLocalization("class_giga_shadow_child", {
+    ["en"] = "Giga Shadow Child"
+})
+translate.AddLocalization("class_asskicker", {
+    ["en"] = "Ass Kicker"
+})
+translate.AddLocalization("class_shitslapper", {
+    ["en"] = "Shit Slapper"
+})
+translate.AddLocalization("class_red_marrow", {
+    ["en"] = "Red Marrow"
+})
+translate.AddLocalization("class_dry_marrow", {
+    ["en"] = "Dry Marrow"
+})
+translate.AddLocalization("class_cold_marrow", {
+    ["en"] = "Cold Marrow"
+})
+translate.AddLocalization("class_ice_marrow", {
+    ["en"] = "Ice Marrow"
+})
+translate.AddLocalization("class_marrow_tyrant", {
+    ["en"] = "Marrow Tyrant"
+})
+
+translate.AddLocalization("class_skeletal_walker", {
+    ["en"] = "Skeletal Walker"
+})
+translate.AddLocalization("class_skeletal_shambler", {
+    ["en"] = "Skeletal Shambler"
+})
+translate.AddLocalization("class_charred_bones", {
+    ["en"] = "Charred Bones"
+})
+translate.AddLocalization("class_skeletal_lurker", {
+    ["en"] = "Skeletal Crawler"
+})
+translate.AddLocalization("class_skeleton_warrior", {
+    ["en"] = "Skeleton Warrior"
+})
+translate.AddLocalization("class_cinder_skeleton_warrior", {
+    ["en"] = "Cinder Skeleton Warrior"
+})
+translate.AddLocalization("class_ashen_skeleton_warrior", {
+    ["en"] = "Ashen Skeleton Warrior"
+})
+translate.AddLocalization("class_skeleton_captain", {
+    ["en"] = "Skeleton Captain"
+})
+translate.AddLocalization("class_deadeye", {
+    ["en"] = "Dead-Eye"
+})
+translate.AddLocalization("class_skeleton_archer", {
+    ["en"] = "Skeleton Archer"
+})
+translate.AddLocalization("class_skeleton_executioner", {
+    ["en"] = "Skeleton Executioner"
+})
+translate.AddLocalization("class_dread_knight", {
+    ["en"] = "Dread Knight"
+})
+translate.AddLocalization("class_dread_shadow", {
+    ["en"] = "Dread Shadow"
+})
+translate.AddLocalization("class_dread_fallen", {
+    ["en"] = "Dread Fallen"
+})
+translate.AddLocalization("class_dread_hulk", {
+    ["en"] = "Dread Hulk"
+})
+translate.AddLocalization("class_dread_grunt", {
+    ["en"] = "Dread Grunt"
+})
+translate.AddLocalization("class_dread_brute", {
+    ["en"] = "Dread Brute"
+})
+translate.AddLocalization("class_dread_bloated", {
+    ["en"] = "Dread Bloated"
+})
+translate.AddLocalization("class_dread_reaper", {
+    ["en"] = "Dread Reaper"
+})
+translate.AddLocalization("class_dread_monger", {
+    ["en"] = "Dread Monger"
+})
+translate.AddLocalization("class_dread_warrior", {
+    ["en"] = "Dread Warrior"
+})
+translate.AddLocalization("class_dreadmammoth", {
+    ["en"] = "Dread Mammoth"
+})
+translate.AddLocalization("class_dread_marshmellow", {
+    ["en"] = "Dread Marshmellow"
+})
+
+translate.AddLocalization("class_ice_demon", {
+    ["en"] = "Ice Demon"
+})
+translate.AddLocalization("class_ice_devil", {
+    ["en"] = "Ice Devil"
+})
+translate.AddLocalization("class_frigid_revenant", {
+    ["en"] = "Frigid Revenant"
+})
+translate.AddLocalization("class_ice_fiend", {
+    ["en"] = "Ice Fiend"
+})
+translate.AddLocalization("class_shadow_lurker", {
+    ["en"] = "Shadow Lurker"
+})
+translate.AddLocalization("class_abyss_lurker", {
+    ["en"] = "Abyss Lurker"
+})
+translate.AddLocalization("class_shadow_walker", {
+    ["en"] = "Shadow Walker"
+})
+translate.AddLocalization("class_lacerator", {
+    ["en"] = "Lacerator"
+})
+translate.AddLocalization("class_ravager", {
+    ["en"] = "Ravager"
+})
+translate.AddLocalization("class_lacerator_charging", {
+    ["en"] = "Charger"
+})
+translate.AddLocalization("class_tormentor", {
+    ["en"] = "Tormentor"
+})
+translate.AddLocalization("class_eradicator", {
+    ["en"] = "Eradicator"
+})
+translate.AddLocalization("class_annihilator", {
+    ["en"] = "Annihilator"
+})
+translate.AddLocalization("class_meteoric_eradicator", {
+    ["en"] = "Meteoric Eradicator"
+})
+
+translate.AddLocalization("class_brood_zombie", {
+    ["en"] = "Brood Zombie"
+})
+translate.AddLocalization("class_brood_queen", {
+    ["en"] = "Brood Queen"
+})
+
+translate.AddLocalization("class_howler", {
+    ["en"] = "Howler"
+})
+translate.AddLocalization("class_lich", {
+    ["en"] = "Lich"
+})
+translate.AddLocalization("class_lesserheresiarch", {
+    ["en"] = "Lesser Heresiarch"
+})
+translate.AddLocalization("class_lessergloomwalker", {
+    ["en"] = "Gloom Walker"
+})
+translate.AddLocalization("class_bloodangel", {
+    ["en"] = "Blood Angel"
+})
+translate.AddLocalization("class_sigil_tyrant", {
+    ["en"] = "Sigil Tyrant"
+})
+translate.AddLocalization("class_heresiarch", {
+    ["en"] = "Heresiarch"
+})
+translate.AddLocalization("class_sanity_sentinel", {
+    ["en"] = "Sanity Sentinel"
+})
+translate.AddLocalization("class_gloomwalker", {
+    ["en"] = "Gloom Strider"
+})
+translate.AddLocalization("class_corruptor", {
+    ["en"] = "Corruptor"
+})
+translate.AddLocalization("class_sigilite", {
+    ["en"] = "Sigilite"
+})
+translate.AddLocalization("class_hate", {
+    ["en"] = "Hate"
+})
+
+translate.AddLocalization("class_gunk_zombie", {
+    ["en"] = "Gunk Zombie"
+})
+translate.AddLocalization("class_gunk_fiend", {
+    ["en"] = "Gunk Fiend"
+})
+translate.AddLocalization("class_tar_monster", {
+    ["en"] = "Tar Monster"
+})
+translate.AddLocalization("class_tar_lard", {
+    ["en"] = "Tar Lard"
+})
+translate.AddLocalization("class_tar_creeper", {
+    ["en"] = "Tar Creeper"
+})
+translate.AddLocalization("class_murk_zombie", {
+    ["en"] = "Murk Zombie"
+})
+translate.AddLocalization("class_rune_watcher", {
+    ["en"] = "Rune Watcher"
+})
+translate.AddLocalization("class_viper", {
+    ["en"] = "Viper"
+})
+translate.AddLocalization("class_parasite", {
+    ["en"] = "Parasite"
+})
+
+translate.AddLocalization("class_propmonster", {
+    ["en"] = "Corrupted Amalgam"
+})
+
+translate.AddLocalization("class_disruptor", {
+    ["en"] = "Disruptor"
+})
+translate.AddLocalization("class_hemorrite", {
+    ["en"] = "Hemorrite"
+})
+translate.AddLocalization("class_flesh_sentinel", {
+    ["en"] = "Flesh Sentinel"
+})
+translate.AddLocalization("class_abhorrent_amalgam", {
+    ["en"] = "Abhorrent Amalgam"
+})
+
+translate.AddLocalization("class_the_lost", {
+    ["en"] = "The Lost"
+})
+translate.AddLocalization("class_the_pursuer", {
+    ["en"] = "The Pursuer"
+})
+translate.AddLocalization("class_spectral_revenant", {
+    ["en"] = "Spectral Revenant"
+})
+translate.AddLocalization("class_fetid_maw", {
+    ["en"] = "Fetid Maw"
+})
+translate.AddLocalization("class_gonome", {
+    ["en"] = "Gonome"
+})
+translate.AddLocalization("class_harbinger", {
+    ["en"] = "Harbinger"
+})
+translate.AddLocalization("class_breachprowler", {
+    ["en"] = "Breach Prowler"
+})
+
+translate.AddLocalization("class_hulk", {
+    ["en"] = "Hulk"
+})
+translate.AddLocalization("class_cinder_thug", {
+    ["en"] = "Cinder Thug"
+})
+
+translate.AddLocalization("class_pigeon", {
+    ["en"] = "Pigeon"
+})
+translate.AddLocalization("class_rat", {
+    ["en"] = "Rat"
+})
+translate.AddLocalization("class_mutant_rat", {
+    ["en"] = "Mutant Rat"
+})
+translate.AddLocalization("class_seagull", {
+    ["en"] = "Seagull"
+})
+
+translate.AddLocalization("class_banished_warrior", {
+    ["en"] = "Banished Warrior"
+})
+translate.AddLocalization("class_restlesszombie", {
+    ["en"] = "Restless Zombie"
+})
+translate.AddLocalization("class_flesh_breaker", {
+    ["en"] = "Flesh Breaker"
+})
+translate.AddLocalization("class_behemoth", {
+    ["en"] = "Behemoth"
+})
+
+translate.AddLocalization("class_deathslinger", {
+    ["en"] = "Deathslinger"
+})
+
+translate.AddLocalization("class_cultist", {
+    ["en"] = "Cultist"
+})
+translate.AddLocalization("class_riot_zombie", {
+    ["en"] = "Riot Zombie"
+})
+translate.AddLocalization("class_enforcer", {
+    ["en"] = "Enforcer"
+})
+translate.AddLocalization("class_bully", {
+    ["en"] = "Bully"
+})
+translate.AddLocalization("class_warden", {
+    ["en"] = "Warden"
+})
+translate.AddLocalization("class_guardian", {
+    ["en"] = "Guardian"
+})
+translate.AddLocalization("class_incinerator", {
+    ["en"] = "Incinerator"
+})
+
+translate.AddLocalization("class_mummy", {
+    ["en"] = "Mummy"
+})
+translate.AddLocalization("class_pharaoh", {
+    ["en"] = "Pharaoh"
+})
+translate.AddLocalization("class_drowner", {
+    ["en"] = "Drowner"
+})
+
+translate.AddLocalization("class_gorefiend", {
+    ["en"] = "Gore Fiend"
+})
+translate.AddLocalization("class_goreslicer", {
+    ["en"] = "Gore Slicer"
+})
+translate.AddLocalization("class_goreripper", {
+    ["en"] = "Gore Ripper"
+})
+translate.AddLocalization("class_goregutter", {
+    ["en"] = "Gore Gutter"
+})
+
+translate.AddLocalization("class_zombie_gunner", {
+    ["en"] = "Zombie Gunner"
+})
+translate.AddLocalization("class_zombie_shotgunner", {
+    ["en"] = "Zombie Shotgunner"
+})
+
+translate.AddLocalization("class_amalgam", {
+    ["en"] = "Amalgam"
+})
+translate.AddLocalization("class_moroi", {
+    ["en"] = "Moroi"
+})
+translate.AddLocalization("class_elemental", {
+    ["en"] = "The Elemental"
+})
+translate.AddLocalization("class_frostbite", {
+    ["en"] = "Frostbite"
+})
+translate.AddLocalization("class_nuclear_terror", {
+    ["en"] = "Nuclear Terror"
+})
+translate.AddLocalization("class_poison_anomaly", {
+    ["en"] = "Poison Anomaly"
+})
+translate.AddLocalization("class_gonarch", {
+    ["en"] = "Gonarch"
+})
+translate.AddLocalization("class_elitezombine", {
+    ["en"] = "Elite Zombine"
+})
+translate.AddLocalization("class_hunter", {
+    ["en"] = "Hunter"
+})
+translate.AddLocalization("class_the_templar", {
+    ["en"] = "The Templar"
+})
+translate.AddLocalization("class_brisket", {
+    ["en"] = "Burnt Brisket"
+})
+translate.AddLocalization("class_poly_mage", {
+    ["en"] = "Poly Mage"
+})
+translate.AddLocalization("class_cremator", {
+    ["en"] = "The Cremator"
+})
+translate.AddLocalization("class_null", {
+    ["en"] = "Null"
+})
+translate.AddLocalization("class_leak", {
+    ["en"] = "Leak"
+})
+translate.AddLocalization("class_tickledecimator", {
+    ["en"] = "Tickle Decimator"
+})
+
+translate.AddLocalization("class_cripple", {
+    ["en"] = "The Cripple"
+})
+translate.AddLocalization("class_winter_walker", {
+    ["en"] = "Winter Walker"
+})
+translate.AddLocalization("class_wendigo", {
+    ["en"] = "Wendigo"
+})
+translate.AddLocalization("class_spellmaster", {
+    ["en"] = "Spellmaster"
+})
+translate.AddLocalization("class_radioactive_zombie", {
+    ["en"] = "Radioactive Zombie"
+})
+translate.AddLocalization("class_burn_victim", {
+    ["en"] = "Burn Victim"
+})
+translate.AddLocalization("class_imp", {
+    ["en"] = "Imp"
+})
+translate.AddLocalization("class_magma_elemental", {
+    ["en"] = "Magma Elemental"
+})
