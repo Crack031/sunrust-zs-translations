fdc303bbc1fe677e0267eeea183da009099a8423
wsvg14 <wsvg14@gmail.com>
2025-10-11 00:03:55 +0800

    zh-CN: ZZZZZEEEEE

--- translations_ex/zombie_escape.lua
+++ translations_ex/zombie_escape.lua
@@ -2,362 +2,448 @@
 
 -- ZOMBIE ESCAPE
 translate.AddLocalization("ZOMBIE_ESCAPE", {
-    ["en"] = "Zombie Escape"
+    ["en"] = "Zombie Escape",
+    ["zh-CN"] = "僵尸逃跑"
 })
 
 translate.AddLocalization("ZE_START", {
-    ["en"] = "Escape from the zombies!"
+    ["en"] = "Escape from the zombies!",
+    ["zh-CN"] = "从尸群中逃出生天!"
 })
 
 -- Class names
 translate.AddLocalization("class_ze_zombie", {
-    ["en"] = "Escape Zombie"
+    ["en"] = "Escape Zombie",
+    ["zh-CN"] = "ZE普通僵尸"
 })
 
 translate.AddLocalization("class_ze_fast_zombie", {
-    ["en"] = "Escape Fast Zombie"
+    ["en"] = "Escape Fast Zombie",
+    ["zh-CN"] = "ZE快速僵尸"
 })
 
 translate.AddLocalization("class_ze_poison_zombie", {
-    ["en"] = "Escape Poison Zombie"
+    ["en"] = "Escape Poison Zombie",
+    ["zh-CN"] = "ZE剧毒僵尸"
 })
 
 translate.AddLocalization("class_ze_ghoul", {
-    ["en"] = "Escape Ghoul"
+    ["en"] = "Escape Ghoul",
+    ["zh-CN"] = "ZE食尸鬼"
 })
 
 -- Class descriptions
 translate.AddLocalization("description_ze_zombie", {
-    ["en"] = "These are only seen in zombie escape."
+    ["en"] = "These are only seen in zombie escape.",
+    ["zh-CN"] = "这些僵尸只在僵尸逃跑中出现."
 })
 
 -- Class control schemes
 translate.AddLocalization("controls_ze_zombie", {
-    ["en"] = "> PRIMARY: Claws\n> SECONDARY: Scream"
+    ["en"] = "> PRIMARY: Claws\n> SECONDARY: Scream",
+    ["zh-CN"] = "> PRIMARY: 攻击\n> SECONDARY: 嚎叫"
 })
 
 translate.AddLocalization("controls_ze_fast_zombie", {
-    ["en"] = "> PRIMARY: Claws\n> SECONDARY: Frenzy\n> RELOAD: Scream"
+    ["en"] = "> PRIMARY: Claws\n> SECONDARY: Frenzy\n> RELOAD: Scream",
+    ["zh-CN"] = "> PRIMARY: 攻击\n> SECONDARY: 狂暴\n> RELOAD: 嚎叫"
 })
 
 translate.AddLocalization("controls_ze_poison_zombie", {
-    ["en"] = "> PRIMARY: Claws\n> SECONDARY: Aegis\n> RELOAD: Scream"
+    ["en"] = "> PRIMARY: Claws\n> SECONDARY: Aegis\n> RELOAD: Scream",
+    ["zh-CN"] = "> PRIMARY: 攻击\n> SECONDARY: 减伤\n> RELOAD: 嚎叫"
 })
 
 translate.AddLocalization("controls_ze_ghoul", {
-    ["en"] = "> PRIMARY: Claws\n> SECONDARY: Scream\n> ON HIT HUMAN: Ghoul Touch"
+    ["en"] = "> PRIMARY: Claws\n> SECONDARY: Scream\n> ON HIT HUMAN: Ghoul Touch",
+    ["zh-CN"] = "> PRIMARY: 攻击\n> SECONDARY: 嚎叫\n> 击中人类时: 易感染状态"
 })
 
 --ZE
 translate.AddLocalization("ZE_WEP_BUY", {
-    ["en"] = ">>> To open the special Zombie Escape menu, press F3!"
+    ["en"] = ">>> To open the special Zombie Escape menu, press F3!",
+    ["zh-CN"] = ">>> 按下 F3 打开 ZE 特殊功能菜单!"
 })
 
 translate.AddLocalization("ZE_AFTER_BUY", {
-    ["en"] = "Purchased %s for %d points! Type !%s to rebuy this weapon."
+    ["en"] = "Purchased %s for %d points! Type !%s to rebuy this weapon.",
+    ["zh-CN"] = "购买 %s 用了 %d 的点数! 输入!%s 重复购买"
 })
 
 translate.AddLocalization("ZE_WEP_EXIST", {
-    ["en"] = "You already have this weapon!"
+    ["en"] = "You already have this weapon!",
+    ["zh-CN"] = "你已经拥有此武器"
 })
 
 translate.AddLocalization("ZE_WEP_COOLDOWN", {
-    ["en"] = "Weapon menu on cooldown. (%ss)"
+    ["en"] = "Weapon menu on cooldown. (%ss)",
+    ["zh-CN"] = "武器菜单正在冷却(%ss)"
 })
 
 translate.AddLocalization("ZE_NADE_EXIST", {
-    ["en"] = "You already have this grenade!"
+    ["en"] = "You already have this grenade!",
+    ["zh-CN"] = "你已经拥有这种手雷了!"
 })
 
 translate.AddLocalization("ZE_NADE_COOLDOWN", {
-    ["en"] = "Your grenade is still on cooldown. (%ss)"
+    ["en"] = "Your grenade is still on cooldown. (%ss)",
+    ["zh-CN"] = "你的手雷购买正在冷却 (%ss)"
 })
 
 translate.AddLocalization("ZE_NADE_STOCK", {
-    ["en"] = "You have %s left in stock."
+    ["en"] = "You have %s left in stock.",
+    ["zh-CN"] = "你还剩 %s 个手雷可供购买"
 })
 
 translate.AddLocalization("ZE_WEP_TITLE", {
-    ["en"] = "Use keybinds to make a selection."
+    ["en"] = "Use keybinds to make a selection.",
+    ["zh-CN"] = "按下键盘相应按键进行选择"
 })
 
 translate.AddLocalization("ZE_ITEM_DROP", {
-    ["en"] = "Press %s to drop your item."
+    ["en"] = "Press %s to drop your item.",
+    ["zh-CN"] = "按 %s 丢弃神器"
 })
 
 translate.AddLocalization("ZE_ITEM_DROP_BIND", {
-    ["en"] = "Bind a key to +zoom to drop your item."
+    ["en"] = "Bind a key to +zoom to drop your item.",
+    ["zh-CN"] = "将按键绑定执行'+zoom'命令以丢弃神器"
 })
 
 translate.AddLocalization("ZE_ITEM_USE", {
-    ["en"] = "Press %s (on the green circle) to use your item."
+    ["en"] = "Press %s (on the green circle) to use your item.",
+    ["zh-CN"] = "按 %s (对准绿圈) 使用神器"
 })
 
 translate.AddLocalization("ZE_ITEM_USE_BIND", {
-    ["en"] = "Bind a key to +use to use your item."
+    ["en"] = "Bind a key to +use to use your item.",
+    ["zh-CN"] = "将按键绑定执行'+use'命令以使用神器"
 })
 
 -- ZE HUD
 -- points/scraps/brains
 translate.AddLocalization("ZE_POINTS", {
-    ["en"] = "Points: %s"
+    ["en"] = "Points: %s",
+    ["zh-CN"] = "点数: %s"
 })
 
 translate.AddLocalization("ZE_SCRAPS", {
-    ["en"] = "Scrap: %s"
+    ["en"] = "Scrap: %s",
+    ["zh-CN"] = "零件: %s"
 })
 
 translate.AddLocalization("ZE_BRAINS", {
-    ["en"] = "Brains: %s / %s"
+    ["en"] = "Brains: %s / %s",
+    ["zh-CN"] = "击杀数: %s / %s",
 })
 
 -- ZE HUD TEXT
 translate.AddLocalization("ZE_HUMANS_T", {
-    ["en"] = "Humans"
+    ["en"] = "Humans",
+    ["zh-CN"] = "人类"
 })
 
 translate.AddLocalization("ZE_ZOMBIES_T", {
-    ["en"] = "Zombies"
+    ["en"] = "Zombies",
+    ["zh-CN"] = "僵尸"
 })
 
 translate.AddLocalization("ZE_HUMAN_T", {
-    ["en"] = "Human"
+    ["en"] = "Human",
+    ["zh-CN"] = "人类"
 })
 
 translate.AddLocalization("ZE_ZOMBIE_T", {
-    ["en"] = "Zombie"
+    ["en"] = "Zombie",
+    ["zh-CN"] = "僵尸"
 })
 
 translate.AddLocalization("ZE_POINT_T", {
-    ["en"] = "Point"
+    ["en"] = "Point",
+    ["zh-CN"] = "点数"
 })
 
 translate.AddLocalization("ZE_POINTS_T", {
-    ["en"] = "Points"
+    ["en"] = "Points",
+    ["zh-CN"] = "点数"
 })
 
 translate.AddLocalization("ZE_SCRAP_T", {
-    ["en"] = "Scrap"
+    ["en"] = "Scrap",
+    ["zh-CN"] = "零件"
 })
 
 translate.AddLocalization("ZE_SCRAPS_T", {
-    ["en"] = "Scraps"
+    ["en"] = "Scraps",
+    ["zh-CN"] = "零件"
 })
 
 translate.AddLocalization("ZE_BRAIN_T", {
-    ["en"] = "Brain"
+    ["en"] = "Brain",
+    ["zh-CN"] = "击杀"
 })
 
 translate.AddLocalization("ZE_BRAINS_T", {
-    ["en"] = "Brains"
+    ["en"] = "Brains",
+    ["zh-CN"] = "击杀"
 })
 
 -- ZE WIN/LOSE
 translate.AddLocalization("ZE_HUMANS_WIN", {
-    ["en"] = "Humans Win"
+    ["en"] = "Humans Win",
+    ["zh-CN"] = "人类胜利"
 })
 
 translate.AddLocalization("ZE_ZOMBIES_WIN", {
-    ["en"] = "Zombies Win"
+    ["en"] = "Zombies Win",
+    ["zh-CN"] = "僵尸胜利"
 })
 
 -- win/lose text
 translate.AddLocalization("ZE_HWIN_TEXT", {
-    ["en"] = "The humans have survived for now."
+    ["en"] = "The humans have survived for now.",
+    ["zh-CN"] = "人类幸存了下来."
 })
 
 translate.AddLocalization("ZE_ZWIN_TEXT", {
-    ["en"] = "The undead army grows stronger."
+    ["en"] = "The undead army grows stronger.",
+    ["zh-CN"] = "僵尸变得更为壮大."
 })
 
 -- ZE ROUND
 translate.AddLocalization("ZE_WARMUP", {
-    ["en"] = "Warm-up: %s"
+    ["en"] = "Warm-up: %s",
+    ["zh-CN"] = "热身时间: %s"
 })
 
 translate.AddLocalization("ZE_DESIRED_ZOMBIES", {
-    ["en"] = "A random group of humans will start as zombies."
+    ["en"] = "A random group of humans will start as zombies.",
+    ["zh-CN"] = "一批人类将会随机选为僵尸"
 })
 
 translate.AddLocalization("ZE_GAME_START", {
-    ["en"] = "The game will start in..."
+    ["en"] = "The game will start in...",
+    ["zh-CN"] = "游戏将开始于..."
 })
 
 -- ZE ADVERT
 translate.AddLocalization("ZE_ADVERT_SCREENSHAKE", {
-    ["en"] = ">>> Tired of screen shake? Type !noshake to disable shaking!"
+    ["en"] = ">>> Tired of screen shake? Type !noshake to disable shaking!",
+    ["zh-CN"] = ">>> 厌倦了屏幕抖动? 输入 !noshake 关闭."
 })
 
 translate.AddLocalization("ZE_ADVERT_WEAPONS", {
-    ["en"] = ">>> You can quick-buy guns by typing !<gunname>. This also works with !he, !ice, !fire, and !upgrade."
+    ["en"] = ">>> You can quick-buy guns by typing !<gunname>. This also works with !he, !ice, !fire, and !upgrade.",
+    ["zh-CN"] = ">>> 可通过输入 !<gunname> 快速购买枪械. 也可输入!he, !ice, !fire, 及 !upgrade 实现快速操作."
 })
 
 translate.AddLocalization("ZE_ADVERT_POINTS", {
-    ["en"] = ">>> Earn points by shooting zombies. Spend them in the shop menu\n>>> To open the weapon shop menu, press F2!"
+    ["en"] = ">>> Earn points by shooting zombies. Spend them in the shop menu\n>>> To open the weapon shop menu, press F2!",
+    ["zh-CN"] = ">>> 通过射击僵尸获取点数以在商店消费\n>>> 按 F2 打开ZE商店菜单!"
 })
 
 translate.AddLocalization("ZE_ADVERT_SCRAP", {
-    ["en"] = ">>> Use scrap to upgrade your weapons. Use it in the Special Menu\n>>> To open the special Zombie Escape menu, press F3!"
+    ["en"] = ">>> Use scrap to upgrade your weapons. Use it in the Special Menu\n>>> To open the special Zombie Escape menu, press F3!",
+    ["zh-CN"] = ">>> 在特殊功能菜单使用零件升级武器\n>>>按 F3 打开ZE特殊功能菜单!"
 })
 
 translate.AddLocalization("ZE_ADVERT_DROPWEP", {
-    ["en"] = ">>> You can drop your guns by pressing 'Z' or 'B'."
+    ["en"] = ">>> You can drop your guns by pressing 'Z' or 'B'.",
+    ["zh-CN"] = ">>> 可尝试按下'Z'或'B'丢弃当前武器."
 })
 
 -- ZE Shop Menu
 translate.AddLocalization("ZE_SHOP_TITLE", {
-    ["en"] = "Zombie Escape - Shop Menu"
+    ["en"] = "Zombie Escape - Shop Menu",
+    ["zh-CN"] = "僵尸逃跑 - 武器商店"
 })
 
 translate.AddLocalization("ZE_SHOP_COST", {
-    ["en"] = "Cost: %s"
+    ["en"] = "Cost: %s",
+    ["zh-CN"] = "价格: %s"
 })
 
 translate.AddLocalization("ZE_SHOP_STOCK", {
-    ["en"] = "Stock: %s"
+    ["en"] = "Stock: %s",
+    ["zh-CN"] = "库存: %s",
 })
 
 translate.AddLocalization("ZE_SHOP_FREE", {
-    ["en"] = "FREE"
+    ["en"] = "FREE",
+    ["zh-CN"] = "免费"
 })
 
 translate.AddLocalization("ZE_SHOPHELPER_CLOSE", {
-    ["en"] = "(ESC / F2) Close"
+    ["en"] = "(ESC / F2) Close",
+    ["zh-CN"] = "(ESC / F2) 关闭",
 })
 
 translate.AddLocalization("ZE_SHOPHELPER_SELECT", {
-    ["en"] = "(KEY_1 to KEY_8) Select Category/Weapon"
+    ["en"] = "(KEY_1 to KEY_8) Select Category/Weapon",
+    ["zh-CN"] = "(数字键1-8) 选择类别/武器"
 })
 
 translate.AddLocalization("ZE_SHOPHELPER_UNSELECT", {
-    ["en"] = "(SHIFT / KEY_9) Unselect Category"
+    ["en"] = "(SHIFT / KEY_9) Unselect Category",
+    ["zh-CN"] = "(SHIFT / 数字键9) 取消类别选择"
 })
 
 translate.AddLocalization("ZE_SHOP_CAT", {
-    ["en"] = "%d. %s"
+    ["en"] = "%d. %s",
+    ["zh-CN"] = "%d. %s"
 })
 
 translate.AddLocalization("ZE_SHOP_CAT_PISTOLS", {
-    ["en"] = "Pistols"
+    ["en"] = "Pistols",
+    ["zh-CN"] = "手枪"
 })
 
 translate.AddLocalization("ZE_SHOP_CAT_SHOTGUNS", {
-    ["en"] = "Shotguns"
+    ["en"] = "Shotguns",
+    ["zh-CN"] = "霰弹枪"
 })
 
 translate.AddLocalization("ZE_SHOP_CAT_SMGS", {
-    ["en"] = "SMGs"
+    ["en"] = "SMGs",
+    ["zh-CN"] = "冲锋枪"
 })
 
 translate.AddLocalization("ZE_SHOP_CAT_RIFLES", {
-    ["en"] = "Rifles"
+    ["en"] = "Rifles",
+    ["zh-CN"] = "突击步枪"
 })
 
 translate.AddLocalization("ZE_SHOP_CAT_SNIPERS", {
-    ["en"] = "Snipers"
+    ["en"] = "Snipers",
+    ["zh-CN"] = "狙击枪"
 })
 
 translate.AddLocalization("ZE_SHOP_CAT_HEAVY", {
-    ["en"] = "Heavy"
+    ["en"] = "Heavy",
+    ["zh-CN"] = "重型武器"
 })
 
 -- ZE SPECIAL MENU
 translate.AddLocalization("ZE_SPECIAL_TITLE", {
-    ["en"] = "Special Items Menu"
+    ["en"] = "Special Items Menu",
+    ["zh-CN"] = "特殊物品菜单"
 })
 
 translate.AddLocalization("ZE_SPECIAL_REBUY", {
-    ["en"] = "Rebuy Last Weapon"
+    ["en"] = "Rebuy Last Weapon",
+    ["zh-CN"] = "重新购买武器"
 })
 
 translate.AddLocalization("ZE_SPECIAL_UPGRADE", {
-    ["en"] = "Upgrade Current Weapon"
+    ["en"] = "Upgrade Current Weapon",
+    ["zh-CN"] = "升级当前武器"
 })
 
 translate.AddLocalization("ZE_SPECIAL_SCRAP_COST", {
-    ["en"] = "Cost: %s Scrap"
+    ["en"] = "Cost: %s Scrap",
+    ["zh-CN"] = "消耗: %s 个零件"
 })
 
 -- ZE LEADER
 translate.AddLocalization("ZE_LEADER_TITLE", {
-    ["en"] = "Leader Menu"
+    ["en"] = "Leader Menu",
+    ["zh-CN"] = "指挥菜单"
 })
 
 translate.AddLocalization("ZE_LEADER_BACK", {
-    ["en"] = "Back"
+    ["en"] = "Back",
+    ["zh-CN"] = "返回"
 })
 
 translate.AddLocalization("ZE_LEADER_EXIT", {
-    ["en"] = "Exit"
+    ["en"] = "Exit",
+    ["zh-CN"] = "退出"
 })
 
 -- leader options
 translate.AddLocalization("ZE_MARKER_MENU", {
-    ["en"] = "Marker Menu"
+    ["en"] = "Marker Menu",
+    ["zh-CN"] = "标点菜单"
 })
 
 translate.AddLocalization("ZE_LEADER_BEACON", {
-    ["en"] = "Toggle Beacon"
+    ["en"] = "Toggle Beacon",
+    ["zh-CN"] = "打开/关闭指挥划线"
 })
 
 translate.AddLocalization("ZE_LEADER_GLOW", {
-    ["en"] = "Toggle Outline Glow"
+    ["en"] = "Toggle Outline Glow",
+    ["zh-CN"] = "打开/关闭指挥高亮"
 })
 
 translate.AddLocalization("ZE_LEADER_TRACERS", {
-    ["en"] = "Toggle Weapon Tracers"
+    ["en"] = "Toggle Weapon Tracers",
+    ["zh-CN"] = "打开/关闭指挥弹道"
 })
 
 translate.AddLocalization("ZE_LEADER_RESIGN", {
-    ["en"] = "Resign from Leader"
+    ["en"] = "Resign from Leader",
+    ["zh-CN"] = "辞任指挥"
 })
 
 -- marker options
 translate.AddLocalization("ZE_LMENU_MARKER", {
-    ["en"] = "Marker"
+    ["en"] = "Marker",
+    ["zh-CN"] = "标点"
 })
 
 translate.AddLocalization("ZE_LMENU_ZMTP", {
-    ["en"] = "Zombie TP"
+    ["en"] = "Zombie TP",
+    ["zh-CN"] = "僵尸传送"
 })
 
 translate.AddLocalization("ZE_LMENU_DEFEND", {
-    ["en"] = "Defend"
+    ["en"] = "Defend",
+    ["zh-CN"] = "防守点"
 })
 
 translate.AddLocalization("ZE_LMENU_CAUTION", {
-    ["en"] = "Caution"
+    ["en"] = "Caution",
+    ["zh-CN"] = "注意"
 })
 
 translate.AddLocalization("ZE_LMENU_CLEAR_RECENT", {
-    ["en"] = "Clear Recent Markers"
+    ["en"] = "Clear Recent Markers",
+    ["zh-CN"] = "清除上一个标点"
 })
 
 translate.AddLocalization("ZE_LMENU_CLEAR_ALL", {
-    ["en"] = "Clear All Markers"
+    ["en"] = "Clear All Markers",
+    ["zh-CN"] = "清除全局标点"
 })
 
 -- LEADER MESSAGES (CLIENT)
 translate.AddLocalization("ZE_LMESSAGE_MARKER", {
-    ["en"] = ">>> Placed a %s marker."
+    ["en"] = ">>> Placed a %s marker.",
+    ["zh-CN"] = ">>> 放置了一个 %s 标点."
 })
 
 translate.AddLocalization("ZE_LMESSAGE_RECENT_MARKER", {
-    ["en"] = ">>> Cleared the most recently placed marker."
+    ["en"] = ">>> Cleared the most recently placed marker.",
+    ["zh-CN"] = ">>> 清除上一个标点."
 })
 
 translate.AddLocalization("ZE_LMESSAGE_ALL_MARKERS", {
-    ["en"] = ">>> Cleared all markers."
+    ["en"] = ">>> Cleared all markers.",
+    ["zh-CN"] = ">>> 清除所有标点."
 })
 
 translate.AddLocalization("ZE_LMESSAGE_BEACON_TOGGLE", {
-    ["en"] = ">>> Toggled beacon %s."
+    ["en"] = ">>> Toggled beacon %s.",
+    ["zh-CN"] = ">>> 打开/关闭指挥划线 %s ."
 })
 
 translate.AddLocalization("ZE_LMESSAGE_GLOW_TOGGLE", {
-    ["en"] = ">>> Toggled outline glow %s."
+    ["en"] = ">>> Toggled outline glow %s.",
+    ["zh-CN"] = ">>> 打开/关闭指挥高亮 %s ."
 })
 
 translate.AddLocalization("ZE_LMESSAGE_TRACERS_TOGGLE", {
-    ["en"] = ">>> Toggled weapon tracers %s."
+    ["en"] = ">>> Toggled weapon tracers %s.",
+    ["zh-CN"] = ">>> 打开/关闭指挥弹道 %s ."
 })
