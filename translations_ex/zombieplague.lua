4adf41d107d3e3fc80cf5468e4c79928db324958
crack <63127055+Crack031@users.noreply.github.com>
2025-10-11 02:43:06 -0400

    more commas

--- translations_ex/zombieplague.lua
+++ translations_ex/zombieplague.lua
@@ -1,10 +1,10 @@
 -- Zombie Plague Mode
 translate.AddLocalization("ZOMBIE_PLAGUE", {
-    ["en"] = "Zombie Plague"
+    ["en"] = "Zombie Plague",
     ["zh-CN"] = "丧尸瘟疫"
 })
 
 translate.AddLocalization("ZP_START", {
-    ["en"] = "Survive the zombie plague!"
+    ["en"] = "Survive the zombie plague!",
     ["zh-CN"] = "从丧尸瘟疫中生存下来!"
 })
